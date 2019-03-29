98c86b722152458b4ce03d367593325c664a68d8
Jakub <kuba.jakub17@gmail.com>
2019-03-29 09:28:16 +0100

    hhhh

--- src/java/NewServlet1.java
+++ src/java/NewServlet1.java
@@ -7,6 +7,7 @@
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -15,16 +16,18 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author Vrtex
  */
-public class NewServlet extends HttpServlet {
+@WebServlet(name = "world", urlPatterns = {"/world"})
+public class NewServlet1 extends HttpServlet {
 
-    private void showForm(HttpServletRequest req, HttpServletResponse response) throws IOException
-    {
-        PrintWriter out = response.getWriter();
-        out.println("<form method=\"get\" action=\"world\">");
-        out.println("<input type=\"text\" name=\"user\" \\>");
-        out.println("<input type=\"submit\" value=\"send\" \\>");
-        out.println("</form>");
-    }
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
     
     
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
@@ -35,10 +38,13 @@ public class NewServlet extends HttpServlet {
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Servlet NewServlet</title>");            
+            out.println("<title>Servlet NewServlet1</title>");            
             out.println("</head>");
             out.println("<body>");
-            showForm(request, response);
+            out.println("hello " + request.getParameter("user"));
+            out.println("<form method=\"get\" action=\"hello\">");
+            out.println("<input type=\"submit\" value=\"send\" \\>");
+            out.println("</form>");
             out.println("</body>");
             out.println("</html>");
         }
